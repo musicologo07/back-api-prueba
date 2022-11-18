eae51a16194d68e3cfd67f4c58ec6005b4932d69
musicologo07 <106623570+musicologo07@users.noreply.github.com>
2022-11-17 23:51:05 -0300

    el quete de jason

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { UploadData } = require("./src/controllers/uploadData");
-const PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.DB_PORT;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
-  server.listen(PORT || 3001, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
   await UploadData();
--- api/src/models/City.js
+++ api/src/models/City.js
@@ -2,27 +2,27 @@ const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-	// defino el modelo
-	sequelize.define(
-		"City",
-		{
-			idCity: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-			},
-			city: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-			provincia: {
-				type: DataTypes.STRING,
-				allowNull: true,
-			},
-		},
-		{
-			timestamps: false,
-			createdAt: false,
-			updatedAd: false,
-		}
-	);
+  // defino el modelo
+  sequelize.define(
+    "City",
+    {
+      idCity: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      provincia: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+      createdAt: false,
+      updatedAd: false,
+    }
+  );
 };
